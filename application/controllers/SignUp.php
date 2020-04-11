a24131ec0bf2edb0fde40c5953d12a9e1c20bcbd
Muhammad Aziz Al-assad <muhazizal.26@gmail.com>
2020-04-11 20:15:14 +0700

    added cdn bootstrap 4, testing, and delete bootstrap local

--- application/controllers/SignUp.php
+++ application/controllers/SignUp.php
@@ -1,4 +1,5 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SignUp extends CI_Controller {
   public function index() {
--- application/views/V_Home.php
+++ application/views/V_Home.php
@@ -6,9 +6,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>TelyuPharmacy | Home</title>
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+	integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<!-- External CSS -->
-	<link rel="stylesheet" href="/assets/css/Home.css">
+	<link rel="stylesheet" href="<?= base_url('assets/css/Home.css')?>">
 
 </head>
 
