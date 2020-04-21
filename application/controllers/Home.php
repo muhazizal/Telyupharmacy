4ae70cd195655c90418c4bc53886540158a6a871
Muhammad Aziz Al-assad <muhazizal.26@gmail.com>
2020-04-21 23:26:25 +0700

    fixed

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -22,15 +22,9 @@ class Home extends CI_Controller {
       }
     }
     
-<<<<<<< HEAD
-    $data['product'] = $this->M_Product->get_AllProduct();
-    $data['article'] = $this->M_Article->get_AllArticle();
-    $this->load->view('V_Profile', $data);
-=======
     $data['products'] = $this->M_Product->get_AllProduct();
     $data['articles'] = $this->M_Article->get_AllArticle();
     $this->load->view('V_Home', $data);
->>>>>>> 650d9a93d847c3af1d3002a7f558c954e2bc15db
   }
 
   public function load_AboutUs() {
