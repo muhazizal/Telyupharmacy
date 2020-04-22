9efebd488473ca44dd7e610a1423041781f5ae20
maulanairfanf <maulanairfanf@gmail.com>
2020-04-22 22:33:17 +0700

    fix admin artile admi product

--- application/views/V_AdminProduct.php
+++ application/views/V_AdminProduct.php
@@ -295,7 +295,7 @@
 
 	<!-- delete product -->
 	<?php foreach($data_product as $product):  ?>
-	<div class="modal fade" id="DeleteArticle<?php echo $product['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal fade" id="DeleteProduct<?php echo $product['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   	<div class="modal-dialog" role="document">
     	<div class="modal-content">
       	<div class="modal-header">
