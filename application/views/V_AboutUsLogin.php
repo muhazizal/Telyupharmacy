c0d8fdc7f0336b59c8b37b7cb3eca37e5a418f26
maulanairfanf <maulanairfanf@gmail.com>
2020-04-24 23:28:45 +0700

    json deskripsi gak keluar

--- application/views/V_AboutUsLogin.php
+++ application/views/V_AboutUsLogin.php
@@ -78,7 +78,9 @@
 
 		<div class="container-fluid" id="section3">
 			<div class="col" id="tittle">Deskripsi</div>
-			<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet praesentium labore deleniti in minima, iure accusantium nemo dolorum animi dignissimos perspiciatis eos non. Natus ullam at, labore reiciendis adipisci quam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum possimus nemo obcaecati magni, aliquid esse. Eum aperiam cupiditate saepe cum, eveniet facere, eos sit, distinctio minima vel ut accusamus a.</p>
+			<table id="deskripsi-table" class="table table-borderless">
+				<!-- Jquery Json Deskripsi -->
+			</table>
 		</div>
 
 		<div class="container-fluid" id="section2">
