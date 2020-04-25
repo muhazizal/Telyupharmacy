c0f17be11692b9f022a0d1f60737faef658d87d8
naufalharsa <naufalharsap@gmail.com>
2020-04-25 12:28:38 +0700

    mode lupdated

--- application/models/M_Product.php
+++ application/models/M_Product.php
@@ -22,12 +22,10 @@ class M_Product extends CI_Model {
   }
 
   public function get_ProductbyPrice($lowprice_product, $highprice_product){
-    if ($highprice_product == null) {
+    if($highprice_product == null || $highprice_product == '' ){
       $this->db->where('price >=', $lowprice_product);
-    } else if ($lowprice_product == null) {
-      $this->db->where('price <=', $highprice_product);
-    } else {
-      $this->db->where('price >=', $lowprice_product);
+    }
+    if($lowprice_product == null || $lowprice_product == '' ){
       $this->db->where('price <=', $highprice_product);
     }
     return $this->db->get('product');
